c2236933b19fd13b4f18ed2f74aa6a959bd3dbcc
阿森 <zhengxs2018@foxmail.com>
2023-12-01 00:50:21 +0800

    不强制覆盖 globalThis 的 WebSocket 对象

--- src/_shims.ts
+++ src/_shims.ts
@@ -1,8 +1,3 @@
 import { WebSocket as NodeWebSocket } from 'ws';
 
-if (!globalThis.WebSocket) {
-  // @ts-expect-error
-  globalThis.WebSocket = NodeWebSocket;
-}
-
-export const WebSocket = globalThis.WebSocket;
+export const WebSocket = globalThis.WebSocket || NodeWebSocket;
