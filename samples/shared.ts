8e6363d4d22da46558e6fa7094e161971456e28a
阿森 <zhengxs2018@foxmail.com>
2023-12-10 20:59:26 +0800

    修改示例图片目录位置

--- samples/shared.ts
+++ samples/shared.ts
@@ -4,23 +4,25 @@ import path from 'node:path';
 import { Readable } from 'node:stream';
 import { ReadableStream } from 'node:stream/web';
 
-export const mediaDir = path.join(__dirname, '..', 'media');
+export const dataDir = path.join(__dirname, 'data');
 
-export function getMediaFile(filename: string) {
-  return createReadStream(path.join(mediaDir, filename));
+export function getTestFile(filename: string) {
+  return createReadStream(path.join(dataDir, filename));
 }
 
 export const cacheDir = path.join(__dirname, '..', '.cache');
 
 mkdirSync(cacheDir, { recursive: true });
 
-export function saveFile(binary: ReadableStream, name?: string) {
+export function saveFile(binary?: globalThis.ReadableStream | ReadableStream | null, name?: string) {
+  if (!binary) return;
+
   const filename = path.resolve(cacheDir, name || `${randomUUID()}.png`);
 
-  Readable.fromWeb(binary).pipe(createWriteStream(filename));
+  Readable.fromWeb(binary as ReadableStream).pipe(createWriteStream(filename));
 }
 
-export async function downloadImage(url: string) {
+export async function downloadImage(url: string, name?: string) {
   const data = await fetch(url).then(response => response.body);
-  saveFile(data as unknown as ReadableStream);
+  saveFile(data, name);
 }
